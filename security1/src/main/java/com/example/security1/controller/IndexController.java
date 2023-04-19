5ee21b95ee304f734ecebd4e99e8a6138e008e90
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-04-20 00:58:08 +0900

    시큐리티컨피그클래스 생성 후 login GetMapping 요청시 시큐리티에서 낚아 채가지 않는 것까지 확인, 특정 인증이 필요한 매핑을 요청했을경우 로그인 페이지로 강제 이동까지 진행

--- security1/src/main/java/com/example/security1/controller/IndexController.java
+++ security1/src/main/java/com/example/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,6 +12,38 @@ public class IndexController {
 		// 머스테치 기본폴더 src/main/resources/
 		// 뷰리졸버 설정 : templates(prefix) .mustache(suffix) 의존성 설정했기에 ->생략이 가능함
 		return "index"; // src/main/resources/templates/index.mustache
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	// 스프링시큐리티가 주소를 낚아채가는중
+	// SecurityConfig 파일에서 권한 설정이후 낚아채지 않음
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨";
 	}
 
 }
