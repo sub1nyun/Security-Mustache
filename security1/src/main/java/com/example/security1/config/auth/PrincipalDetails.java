1ccc4bf37cd99bfbd295c4a268d0afa8261920a3
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-04-26 00:45:42 +0900

    스프링 시큐리티 이해하기 2차

--- security1/src/main/java/com/example/security1/config/auth/PrincipalDetails.java
+++ security1/src/main/java/com/example/security1/config/auth/PrincipalDetails.java
@@ -1,4 +1,4 @@
-package com.example.security1.config.auth;
+package com.example.security1.config.auth; 
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,9 +25,12 @@ public class PrincipalDetails implements UserDetails {
 	}
 	
 	// 해당 User의 권한을 리턴하는 곳!
+	// 계정의 권한을 리턴함 -> 계정이 갖고있는 권한이 무엇인지
+	// 계정의 권한이 여러개 있을 수 있어서 루프를 돌아야 하기때문에 컬렉션
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		Collection<GrantedAuthority> collect = new ArrayList<>(); // ArrayList 컬렉션의 자식
+		//collect.add(()->{return user.getRole();}); 람다식으로 표현하기 
 		collect.add(new GrantedAuthority() {
 			
 			@Override
@@ -48,24 +51,24 @@ public class PrincipalDetails implements UserDetails {
 		return user.getUsername();
 	}
 
-
+	// 계정이 만료되지 않았는지를 리턴함 (ture: 만료 안 됨)
 	@Override
 	public boolean isAccountNonExpired() {
 		// 계정 만료
 		return true;
 	}
-
+	// 계정이 잠겨져 있는지를 리턴 true : 잠기지 않음
 	@Override
 	public boolean isAccountNonLocked() {
 		// 계정 잠김
 		return true;
 	}
-
+	// 비밀번호가 만료되지 않았는지를 리턴함 (true : 만료 안 됨)
 	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
-
+	// 계정 활성화 여부 리턴
 	@Override
 	public boolean isEnabled() {
 		
