b9e67a20d986e6a240c4bf95d37f5cc53717583f
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-07-02 00:19:51 +0900

    JWT 구조 및 RSA이해, 프로젝트 생성 후 라이브러리 추가

---
+++ jwt-second/src/main/java/com/sby/second/JwtSecondApplication.java
@@ -0,0 +1,13 @@
+package com.sby.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecondApplication.class, args);
+	}
+
+}
